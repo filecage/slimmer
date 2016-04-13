b17ecb17eb1e9a3dc4f85ede48dd3287f5492101
David Beuchert <mail@dbeuchert.com>
2016-04-13 17:07:41 +0200

    creator injection trait for items that require DI

---
+++ lib/CreatorInjectionTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+    namespace Slimmer;
+
+    use Creator\Creator;
+
+    trait CreatorInjectionTrait {
+
+        /**
+         * @var Creator
+         */
+        private $creator;
+
+        /**
+         * @param Creator $creator
+         *
+         * @return $this
+         */
+        function injectCreator (Creator $creator) {
+            $this->creator = $creator;
+
+            return $this;
+        }
+
+        /**
+         * @return Creator
+         */
+        function getCreator () {
+            return $this->creator;
+        }
+        
+    }
