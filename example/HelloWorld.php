2d028ddc2ce6a25b819d07891191799bf89cb8a2
David Beuchert <mail@dbeuchert.com>
2016-04-14 21:27:59 +0200

    updates example with variables

--- example/HelloWorld.php
+++ example/HelloWorld.php
@@ -4,9 +4,10 @@
 
     $app = new \Slimmer\App();
 
-    $app->getRouter()->registerGetHandler('(.*)', function (\Slimmer\Buffer $buffer) {
+    $app->getRouter()->registerGetHandler('/hello/{(.*)variable}', function (\Slimmer\Buffer $buffer, \Slimmer\Arguments $arguments) {
         $buffer->hello = 'world';
         $buffer->timestamp = time();
+        $buffer->variable = $arguments->get('variable');
     });
 
     $app->run();
