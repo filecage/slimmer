fed9d29aa31ab03411f530f15db3bfbeca1720ff
David Beuchert <mail@dbeuchert.com>
2016-04-14 22:26:46 +0200

    updates example

--- example/HelloWorld.php
+++ example/HelloWorld.php
@@ -4,10 +4,13 @@
 
     $app = new \Slimmer\App();
 
-    $app->getRouter()->registerGetHandler('/hello/{(.*)variable}', function (\Slimmer\Buffer $buffer, \Slimmer\Arguments $arguments) {
-        $buffer->hello = 'world';
-        $buffer->timestamp = time();
-        $buffer->variable = $arguments->get('variable');
-    });
+    $app->getRouter()
+        ->registerGetHandler('/hello/{(.*)variable}', function (\Slimmer\Buffer $buffer, \Slimmer\Arguments $arguments) {
+            $buffer->hello = 'world';
+            $buffer->timestamp = time();
+            $buffer->variable = $arguments->get('variable');
+        })->registerGetHandler(null, function(){
+            return 'I am a default route with a body merged into the global buffer.';
+        });
 
     $app->run();
